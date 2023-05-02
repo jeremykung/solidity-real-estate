c30174f72a92bcc827c70792e0924786c7019d90
jeremykung <jeremykung@gmail.com>
2023-05-02 13:36:12 +0800

    updated frontend, bug with totalSupply

--- src/App.js
+++ src/App.js
@@ -15,22 +15,62 @@ import config from './config.json';
 
 function App() {
 
-  // const loadBlockchainData = async () => {
-  //   const provider = new ethers.providers.Web3Provider(window.ethereum)
-  //   const accounts = await window.ethereum.request({ method: 'eth_requestAccounts'})
-  //   console.log(accounts)
-  // }
+  const [provider, setProvider] = useState(null)
+  const [account, setAccount] = useState(null)
 
-  // useEffect(() => {
-  //   loadBlockchainData()
-  // }, [])
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+
+    const network = await provider.getNetwork()
+
+    const realEstate = new ethers.Contract(config[network.chainId].realEstate.address, RealEstate, provider)
+    const totalSupply = await realEstate.totalSupply() // bug coming from here in Console
+    // console.log('homes total supply:', totalSupply)
+
+    // config[network.chainId].realEstate.address
+    // config[network.chainId].escrow.address
+
+    // console.log('realEstate Address:', config[network.chainId].realEstate.address)
+    // console.log('escrow Address:', config[network.chainId].escrow.address)
+
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
+      const account = ethers.utils.getAddress(accounts[0])
+      setAccount(account)
+    })
+
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
 
   return (
     <div>
 
+      <Navigation account={account} setAccount={setAccount} />
+      <Search />
       <div className='cards__section'>
 
-        <h3>Welcome to Millow!</h3>
+        <h3>Homes for you</h3>
+        <hr />
+        <div className='cards'>
+          <div className='card'>
+            <div className='card__image'>
+              <img src="" alt='Home' />
+            </div>
+            <div className='card__info'>
+              <h4>1 ETH</h4>
+              <p>
+                <strong>1</strong> beds |
+                <strong>2</strong> baths |
+                <strong>3</strong> sqft |
+              </p>
+              <p>123 Elm St</p>
+            </div>
+          </div>
+        </div>
 
       </div>
 
